eb3dbf5f6796eead21d8098aff587655b2a3b44b
Muhammad Ali <solangimuhammadali676@gmai.com>
2026-01-06 21:07:23 -0800

    this is my 2 class assignment!

---
+++ index2.py
@@ -0,0 +1,18 @@
+#print("Hello World")
+#print("1"+"1")
+#print("Hello this is M Ali,\nand we're in web dev with python batch \nI am from UK.\nI am study in federal university. \nI live in current Rabia Petal. \nI am part tme course in saylani")
+
+# hello=123
+# print(hello+3)
+# print("hello equal to:",hello)
+# num1=50
+# num2=100
+# print("This is Addition",num1+num2,"\nThis is Subtraction:",num1-num2,"\nThis is Multiply:",num1*num2,"\nThis is Divide:",num1/num2,)
+
+# input / output in python !
+
+num1=int(input("Enter a number:")) 
+# charc of input, bydefault considers value as string,
+print("This is num1:",num1+num1)
+
+
